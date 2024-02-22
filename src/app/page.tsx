51c1c75920c32e6bbe41d53aa0559299ec71c797
Arnabh <sinhaarnabh888@gmail.com>
2024-02-23 00:05:13 +0530

    Course Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { FeaturedCourses } from "@/components/FeaturedCourses";
 import { HeroSection } from "@/components/HeroSection";
 import Image from "next/image";
 
@@ -6,6 +7,7 @@ export default function Home() {
   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
   
    <HeroSection />
+   <FeaturedCourses />
   </main>
   );
 }
