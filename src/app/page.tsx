44e8bdb68f4dca4cee049e5bdcf3fdbd4b1d3fc0
Arnabh <sinhaarnabh888@gmail.com>
2024-02-22 23:34:37 +0530

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { HeroSection } from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-   <h1 className="text-2xl text-center">Music App</h1> 
+  
+   <HeroSection />
   </main>
   );
 }
