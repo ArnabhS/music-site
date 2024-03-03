5a60f9c959b3ce69873af03c5efaa479e6f99077
Arnabh <sinhaarnabh888@gmail.com>
2024-03-03 17:40:26 +0530

    card hover effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { FeaturedCourses } from "@/components/FeaturedCourses";
 import { HeroSection } from "@/components/HeroSection";
 import MusicSchoolTestimonial from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
@@ -12,6 +13,7 @@ export default function Home() {
    <FeaturedCourses />
    <WhyChooseUs />
    <MusicSchoolTestimonial/>
+   <UpcomingWebinars />
   </main>
   );
 }
