ee76e558ab6c66a6c0efdc50bf762b466b6d2a4c
Arnabh <sinhaarnabh888@gmail.com>
2024-03-03 18:01:38 +0530

    instructor section add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { FeaturedCourses } from "@/components/FeaturedCourses";
 import { HeroSection } from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonial from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -14,6 +15,7 @@ export default function Home() {
    <WhyChooseUs />
    <MusicSchoolTestimonial/>
    <UpcomingWebinars />
+   <Instructors/>
   </main>
   );
 }
