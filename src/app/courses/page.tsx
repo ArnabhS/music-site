5c7bbad5b410fecdd32c87ba9b288de725d71cbc
Arnabh <sinhaarnabh888@gmail.com>
2024-03-27 12:41:56 +0530

    fixes

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -10,7 +10,7 @@ function page() {
         <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">All courses ({courseData.courses.length})</h1>  
         <div className="flex flex-wrap justify-center">
             {courseData.courses.map((course) => (
-                <CardContainer className="inter-var m-4">
+                <CardContainer className="inter-var m-4" key={course.id}>
                 <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
                   <CardItem
                     translateZ="50"
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -18,7 +18,7 @@ export const HeroSection = () => {
             >Master The Art of Music</h1>
             <p
               className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto"
-            >Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
+            >Dive into our comprehensive music courses and transform your musical journey today. Whether you are a beginner or looking to refine your skills, join us to unlock your true potential.</p>
             <div className='mt-4'>
                 <Link href='/courses'>
                    <Button
